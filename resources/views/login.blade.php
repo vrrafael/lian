dd3d9bd43bc11da5a41d7017e7b3f96afb7b85e3
rafael.ribeiro <vieira.rrafael@outlook.com>
2021-04-02 08:48:48 -0400

    Mensagem de erro em login
    Mensagem de erro foi adicionado à página de login quando email o senha não existem.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,18 +3,26 @@
 @section('title', 'Login')
 
 @section('content')
-    <form action="{{ route('authenticate') }}" method="POST" class="col s8 offset-s2">
-        <div class="row">
-            <div class="input-field col s12">
-                <input name="email" id="email" type="email" class="validate">
-                <label for="email">E-mail</label>
+    <div class="row">
+        <form action="{{ route('authenticate') }}" method="POST" class="col s8 offset-s2">
+            <div class="row">
+                <div class="input-field col s12">
+                    <input name="email" id="email" type="email" class="validate">
+                    <label for="email">E-mail</label>
+                </div>
+                <div class="input-field col s12">
+                    <input id="password" type="password" name="password">
+                    <label for="password">Senha</label>
+                </div>
             </div>
-            <div class="input-field col s12">
-                <input id="password" type="password" name="password">
-                <label for="password">Senha</label>
-            </div>
-        </div>
-        <div class="row"><button class="btn waves-effect waves-light orange darken-4" type="submit" name="action">Login</button></div>
-        @csrf
-    </form>
+            <div class="row"><button class="btn waves-effect waves-light orange darken-4" type="submit"
+                    name="action">Login</button></div>
+            @csrf
+        </form>
+    </div>
+    <div class="row">
+        @error('email')
+            <div class="col s12">{{$message}}</div>
+        @enderror
+    </div>
 @endsection
