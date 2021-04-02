f081893ee9ca3731033c321cad8ea614a6bf9586
rafael.ribeiro <vieira.rrafael@outlook.com>
2021-04-02 10:08:18 -0400

    Model e migration para tabela projetos

---
+++ app/Models/Projeto.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Projeto extends Model
+{
+    use HasFactory;
+
+    protected $table = 'projetos';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nome',
+        'dt_inicio',
+        'dt_fim',
+        'valor',
+        'risco',
+    ];
+
+}
---
+++ database/migrations/2021_04_02_132956_create_projetos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjetosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projetos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 100);
+            $table->date('dt_inicio');
+            $table->date('dt_fim');
+            $table->float('valor', 8, 2);
+            $table->char('risco', 6);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projetos');
+    }
+}
