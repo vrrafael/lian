2474dc94b3218bb4efca1e666ae0efc4f0a14eb7
rafael.ribeiro <vieira.rrafael@outlook.com>
2021-04-02 14:38:07 -0400

    UserSeed e UserFactory
    os arquivos foram modificados para criar um número maior de usuários

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
@@ -14,30 +14,15 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $dados = [
-            [
-                'name' => 'Policarpo Quaresma',
-                'email' => 'policarpo@loremipsum.com',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-                // 'remember_token' => \Illuminate\Support\Str::random(10),
-            ],
-            [
-                'name' => 'Maria Capitolina',
-                'email' => 'capitolina@loremipsum.com',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password                
-            ],
-            [
-                'name' => 'Pedro Alcantara',
-                'email' => 'pedro@loremipsum.com',
-                'email_verified_at' => now(),
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password                
-            ],
+        $user = [
+            'name' => 'Policarpo Quaresma',
+            'email' => 'policarpo@loremipsum.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
 
-        foreach($dados as $user){
-            (new User())->fill($user)->save();
-        }
+        (new User())->fill($user)->save();
+
+        User::factory(10)->create();
     }
 }
